49009394ed99d409dcf5809a1c53512e61457b1e
Prit Kasundra <pritkasundra229@gmail.com>
2026-03-24 23:32:19 +0530

    Add Professor Model Class

---
+++ src/main/java/com/university/courseRegistrationSystem/model/Professor.java
@@ -0,0 +1,32 @@
+package com.university.courseRegistrationSystem.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "professors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Professor {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String Name;
+
+    @Column(nullable = false,unique = true)
+    private String Email;
+
+    @Column(nullable = false)
+    private String Password;
+
+    @OneToMany(mappedBy = "professor", fetch = FetchType.LAZY)
+    private List<Course> courses = new ArrayList<>();
+}
