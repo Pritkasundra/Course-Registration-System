fd1020280a0ff349890bd7e08b5fbbe767974773
Prit Kasundra <pritkasundra229@gmail.com>
2026-03-29 16:32:45 +0530

    Add updateCgpaAndCredits Method

--- src/main/java/com/university/courseRegistrationSystem/repository/StudentRepository.java
+++ src/main/java/com/university/courseRegistrationSystem/repository/StudentRepository.java
@@ -1,8 +1,13 @@
 package com.university.courseRegistrationSystem.repository;
 import com.university.courseRegistrationSystem.model.Student;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long>{
@@ -11,4 +16,10 @@ public interface StudentRepository extends JpaRepository<Student,Long>{
 
     // To check whether Student which given email is exist or not
     boolean existsByEmail(String email);
+
+    // update student CGPA and completed credits after grading
+    @Modifying
+    @Transactional
+    @Query("UPDATE Student s SET s.cgpa = :cgpa, " + "s.completedCredits = :completedCredits WHERE s.id = :studentId")
+    void updateCgpaAndCredits(@Param("studentId") Long studentId, @Param("cgpa") BigDecimal cgpa, @Param("completedCredits") int completedCredits);
 }
