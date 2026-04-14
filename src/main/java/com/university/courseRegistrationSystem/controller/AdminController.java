0a81d139d29e9d06b611ea3c9c7b58f6e5191a29
Prit Kasundra <pritkasundra229@gmail.com>
2026-04-14 17:02:13 +0530

    refactor hasrole to hasAuthority

--- src/main/java/com/university/courseRegistrationSystem/controller/AdminController.java
+++ src/main/java/com/university/courseRegistrationSystem/controller/AdminController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasAuthority('ADMIN')")
 
 public class AdminController {
     private final AdminService adminService;
